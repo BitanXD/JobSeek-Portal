62a437b2a07c6ea94456b712849a2c9ef360e194
Bitan Sarkar <bitansarkar088@gmail.com>
2025-06-07 15:54:14 +0530

    added companies marque

--- job-portal-frontend/src/LandingPage/DreamJob.tsx
+++ job-portal-frontend/src/LandingPage/DreamJob.tsx
@@ -1,20 +1,63 @@
+import { Avatar, TextInput } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
+
 const DreamJob = () => {
   return (
     <div className="flex items-center px-16">
       <div className="flex flex-col w-[45%] gap-3">
         <div className="text-6xl font-bold leading-tight text-mine-shaft-100">
-            Find your <span className="text-bright-sun-400">dream job</span> with us
+          Find your <span className="text-bright-sun-400">dream job</span> with
+          us
         </div>
         <div className="text-lg text-mine-shaft-200">
-            Good life begins with a good company. Start exploring thousands of job in one place.
+          Good life begins with a good company. Start exploring thousands of job
+          in one place.
         </div>
-        <div>
-
+        <div className="flex gap-3 mt-5">
+          <TextInput
+            className="bg-mine-shaft-900 rounded-lg p-1 px-2 text-mine-shaft-100 [&_input]:!text-mine-shaft-100"
+            variant="unstyled"
+            label="Job Title"
+            placeholder="Software Engineer"
+          />
+          <TextInput
+            className="bg-mine-shaft-900 rounded-lg p-1 px-2 text-mine-shaft-100 [&_input]:!text-mine-shaft-100"
+            variant="unstyled"
+            label="Job Type"
+            placeholder="Full Time"
+          />
+          <div className="flex justify-center items-center h-full w-20 bg-bright-sun-400 text-mine-shaft-100 rounded-lg p-2 hover:bg-bright-sun-500 cursor-pointer">
+            <IconSearch stroke={2} className="h-[85%] w-[85%]" />
+          </div>
         </div>
       </div>
       <div className="w-[55%] flex items-center justify-center">
-        <div className="w-[30rem]">
-            <img src="boy.png" alt="boy.png" />
+        <div className="w-[30rem] relative">
+          <img src="boy.png" alt="boy.png" />
+          <div className="w-fit border-bright-sun-400 border rounded-lg p-2 backdrop-blur-md top-[50%] -right-10 absolute">
+            <div className="text-center mb-1 text-sm text-mine-shaft-100 ">10K+ got jobs</div>
+            <Avatar.Group>
+              <Avatar src="avatar.png" />
+              <Avatar src="avatar1.png" />
+              <Avatar src="avatar2.png" />
+              <Avatar>+9K</Avatar>
+            </Avatar.Group>
+          </div>
+          <div className="w-fit border-bright-sun-400 border rounded-lg p-2 backdrop-blur-md top-[26%] -left-6 absolute gap-3 flex flex-col">
+            <div className="flex gap-2 items-center">
+              <div className="w-10 h-10 p-1 bg-mine-shaft-900 rounded-lg">
+                <img src="/Google.png" alt="" />
+              </div>
+              <div className="text-sm text-mine-shaft-100">
+                <div>Software Engineer</div>
+                <div className="text-mine-shaft-200 text-xs">New York</div>
+              </div>
+            </div>
+            <div className="flex justify-around gap-2 text-mine-shaft-200 text-xs">
+              <span>1 day ago</span>
+              <span>120 Applicants</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ job-portal-frontend/src/LandingPage/JobCategory.tsx
@@ -0,0 +1,22 @@
+const JobCategory = () => {
+  return (
+    <div className="mt-20 pb-5">
+      <div className="text-4xl text-center mb-3 font-semibold text-mine-shaft-100">
+        Browse <span className="text-bright-sun-400">Job</span> Category
+      </div>
+      <div className="text-lg text-mine-shaft-300 text-center w-1/2 mx-auto">
+        Explore diverse job opportunities tailored to your skills. Start your career joruney today!
+      </div>
+      <div>
+        <div>
+            <img src="/Category/Digital Marketing.png" alt="" />
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default JobCategory;
