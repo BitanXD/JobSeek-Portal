b35ad2359de57774ee74735f5f0888762c0cf2a2
BitanXD <bitansarkar088@gmail.com>
2025-06-10 00:02:38 +0530

    added a carousel in landing page

--- job-portal-frontend/src/LandingPage/JobCategory.tsx
+++ job-portal-frontend/src/LandingPage/JobCategory.tsx
@@ -1,20 +1,47 @@
+import { Carousel } from "@mantine/carousel";
+import { jobCategory } from "../Data/Data";
+
 const JobCategory = () => {
   return (
     <div className="mt-20 pb-5">
       <div className="text-4xl text-center mb-3 font-semibold text-mine-shaft-100">
         Browse <span className="text-bright-sun-400">Job</span> Category
       </div>
-      <div className="text-lg text-mine-shaft-300 text-center w-1/2 mx-auto">
-        Explore diverse job opportunities tailored to your skills. Start your career joruney today!
+      <div className="text-lg mb-10 text-mine-shaft-300 text-center w-1/2 mx-auto">
+        Explore diverse job opportunities tailored to your skills. Start your
+        career joruney today!
       </div>
-      <div className="flex flex-col items-center w-64">
-        <div className="p-2 bg-bright-sun-300 rounded-full">
-            <img className="h-8 w-8" src="/Category/Digital Marketing.png" alt="" />
-        </div>
-        <div className="text-mine-shaft-100 text-xl font-semibold">Digital Marketting.</div>
-        <div className="text-sm text-center text-mine-shaft-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, ipsa.</div>
-        <div className="text-bright-sun-300 text-lg">1K+ new jobs posted</div>
-      </div>
+      <Carousel
+        slideSize="22%"
+        slideGap="md"
+        controlsOffset="sm"
+        controlSize={26}
+        withControls
+        withIndicators={false}
+      >
+        {jobCategory.map((category, index) => (
+          <Carousel.Slide>
+            <div className="flex flex-col items-center w-64">
+              <div className="p-2 bg-bright-sun-300 rounded-full">
+                <img
+                  className="h-8 w-8"
+                  src={`/Category/${category.name}.png`}
+                  alt={category.name}
+                />
+              </div>
+              <div className="text-mine-shaft-100 text-xl font-semibold">
+                {category.name}
+              </div>
+              <div className="text-sm text-center text-mine-shaft-300">
+                {category.desc}
+              </div>
+              <div className="text-bright-sun-300 text-lg">
+                {category.jobs}+ new jobs posted
+              </div>
+            </div>
+          </Carousel.Slide>
+        ))}
+      </Carousel>
     </div>
   );
 };
