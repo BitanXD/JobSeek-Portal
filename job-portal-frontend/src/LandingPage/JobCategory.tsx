11b82f0168a68f10cb2d19a7c33cf16ebf116636
BitanXD <bitansarkar088@gmail.com>
2025-06-13 00:07:37 +0530

    added styling, added a new section

--- job-portal-frontend/src/LandingPage/JobCategory.tsx
+++ job-portal-frontend/src/LandingPage/JobCategory.tsx
@@ -1,5 +1,6 @@
 import { Carousel } from "@mantine/carousel";
 import { jobCategory } from "../Data/Data";
+import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 
 const JobCategory = () => {
   return (
@@ -18,10 +19,13 @@ const JobCategory = () => {
         controlSize={26}
         withControls
         withIndicators={false}
+        nextControlIcon={<IconArrowRight className="h-8 w-8" />}
+        previousControlIcon={<IconArrowLeft className="h-8 w-8" />}
+        className="focus-visible:[&_button]:!outline-none [&_button]:!bg-bright-sun-400 [&_button]:!border-none [&_button]:hover:!opacity-75 [&_button]:opacity-0 hover:[&_button]:!opacity-100"
       >
         {jobCategory.map((category, index) => (
           <Carousel.Slide>
-            <div className="flex flex-col items-center w-64">
+            <div className="flex flex-col items-center w-64 gap-2 border border-bright-sun-400 p-5 rounded-xl hover:cursor-pointer hover:shadow-[0_0_5px_2px_black] !shadow-bright-sun-400 my-5 transition duration-300 ease-in-out">
               <div className="p-2 bg-bright-sun-300 rounded-full">
                 <img
                   className="h-8 w-8"
---
+++ job-portal-frontend/src/LandingPage/Working.tsx
@@ -0,0 +1,14 @@
+const Working = () => {
+  return (
+    <div className="mt-20 pb-5">
+      <div className="text-4xl text-center mb-3 font-semibold text-mine-shaft-100">
+        How it <span className="text-bright-sun-400">Works</span>
+      </div>
+      <div className="text-lg mb-10 text-mine-shaft-300 text-center w-1/2 mx-auto">
+        Effortlessly navigate through the process and land your dream job.
+      </div>
+    </div>
+  );
+};
+
+export default Working;
