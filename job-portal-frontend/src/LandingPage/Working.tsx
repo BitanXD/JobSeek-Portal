05c8abd7945bd057e53577cc59b1a3e5816893ea
BitanXD <bitansarkar088@gmail.com>
2025-08-28 02:39:24 +0530

    updated landing page, added resume part

--- job-portal-frontend/src/LandingPage/Working.tsx
+++ job-portal-frontend/src/LandingPage/Working.tsx
@@ -1,3 +1,4 @@
+import {work } from '../Data/Data'
 const Working = () => {
   return (
     <div className="mt-20 pb-5">
@@ -6,6 +7,24 @@ const Working = () => {
       </div>
       <div className="text-lg mb-10 text-mine-shaft-300 text-center w-1/2 mx-auto">
         Effortlessly navigate through the process and land your dream job.
+      </div>
+      <div className="flex px-16 justify-between items-center">
+        <div className="">
+          <img className="w-[30rem]" src="/Working/Girl.png" alt="girl" />
+        </div>
+        <div className="flex flex-col gap-10">
+          {work.map((item, index) => (
+            <div key={index} className="flex items-center gap-4">
+            <div className="p-2.5 bg-bright-sun-300 rounded-full">
+              <img className="h-12 w-12" src={`/Working/${item.name}.png`} alt={item.name} />
+            </div>
+            <div>
+              <div className="text-mine-shaft-200 text-xl font-semibold">{item.name}</div>
+              <div className="text-mine-shaft-300">{item.desc}</div>
+            </div>
+          </div>
+          ))}
+        </div>
       </div>
     </div>
   );
